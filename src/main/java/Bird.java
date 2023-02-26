ae1612350eb0beeeca23fb96d93bb15eca8c27bf
melis-a <rampersaudmelisa@gmail.com>
2023-02-26 21:40:53 +0000

    Completed Bird Lab

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -33,19 +33,29 @@ public class Bird {
      */
     public int ageMonths;
 
+
     /**
      * A constructor for the bird class: this method allows us to build a new bird with the 'new' keyword.
      * No need to change anything here, for now.
      */
     public Bird(){
-
+       
     }
     /**
      * @return "Tweet" if ageMonths is less than 3 and "Chirp" if ageMonths is
      *         greater than or equal to 3.
      */
     public String sound(){
-        return "";
+        if (ageMonths < 3) {
+            return "Tweet";
+
+        } 
+        else if (ageMonths >= 3){
+
+        }
+
+
+        return "Chirp";
     }
 
 }
